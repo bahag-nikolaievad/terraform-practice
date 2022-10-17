2c28c3d0375e1106d06c66a4a29f14c06d57edfc
bahag-nikolaievad <dariia.nikolaieva@bahag.com>
2022-10-17 11:27:10 +0200

    change fmt

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
+      source = "hashicorp/google"
     }
   }
 }
@@ -12,22 +12,22 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "server" {
-    name          = "bahag-vpc-server"
-    ip_cidr_range = "10.0.0.0/24"
-    region        = var.region
-    network       = google_compute_network.vpc_network.id
+  name          = "bahag-vpc-server"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "database" {
-    name          = "bahag-vpc-database"
-    ip_cidr_range = "10.0.1.0/24"
-    region        = var.region
-    network       = google_compute_network.vpc_network.id
+  name          = "bahag-vpc-database"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_subnetwork" "database2" {
-    name          = "bahag-vpc-database2"
-    ip_cidr_range = "10.0.1.0/24"
-    region        = var.region
-    network       = google_compute_network.vpc_network.id
+  name          = "bahag-vpc-database2"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
 }
