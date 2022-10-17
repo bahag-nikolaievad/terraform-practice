682f645add8a55ac78fd369694d93ab25e773d99
bahag-nikolaievad <dariia.nikolaieva@bahag.com>
2022-10-17 11:22:08 +0200

    del comments

--- providers.tf
+++ providers.tf
@@ -1,13 +1,5 @@
 provider "google" {
   project = var.project
-  # credentials = file("/Users/nikolaievad/Downloads/doit-sandbox-20220613-kbwhxs-87ba948b979a.json")
   region  = var.region
   zone    = var.zone
-}
-
-terraform {
-  # backend "gcs" {
-  #   bucket = "gcpprojectid-tfstate"
-  #   prefix = "terraform/state"
-  # }
 }
