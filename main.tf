d20cd0be930948e2a1ff42a968399c965711fbfa
bahag-nikolaievad <dariia.nikolaieva@bahag.com>
2022-10-17 11:29:33 +0200

    new subnet

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_compute_subnetwork" "database" {
 
 resource "google_compute_subnetwork" "database2" {
   name          = "bahag-vpc-database2"
-  ip_cidr_range = "10.0.1.0/24"
+  ip_cidr_range = "10.0.2.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
 }
