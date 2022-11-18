b7d370fc3677b00888801249ca6e6eec7f5103f0
bahag-nikolaievad <dariia.nikolaieva@bahag.com>
2022-11-18 09:22:07 +0100

    add policy

--- main.tf
+++ main.tf
@@ -53,16 +53,16 @@ resource "google_cloud_run_service" "cloud_run" {
   }
 }
 
-# data "google_iam_policy" "noauth" {
-#   binding {
-#     role    = "roles/run.invoker"
-#     members = ["allUsers"]
-#   }
-# }
+data "google_iam_policy" "noauth" {
+  binding {
+    role    = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
 
-# resource "google_cloud_run_service_iam_policy" "noauth" {
-#   location = google_cloud_run_service.location
-#   service  = google_cloud_run_service.name
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.location
+  service  = google_cloud_run_service.name
 
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
