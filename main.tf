9957ee7ee1cf582757fa27624135ff11887a0f28
bahag-nikolaievad <dariia.nikolaieva@bahag.com>
2022-11-18 09:27:27 +0100

    add policy

--- main.tf
+++ main.tf
@@ -61,9 +61,8 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.default.location
-  project  = google_cloud_run_service.default.project
-  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.cloud_run.location
+  service  = google_cloud_run_service.cloud_run.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
